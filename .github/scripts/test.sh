9253c0d4bb598795d16750f4e32091d5fbc23d9f
IbrahimAdereni-socuimtech <ibrahim.aldereni@optimumpartners.co>
2024-10-21 14:43:08 +0300

    test GA

---
+++ .github/scripts/test.sh
@@ -0,0 +1,9 @@
+if [ "$EVENT_NAME" == "push" ]; then
+  echo "ENVIRONMENT=qa" >> $GITHUB_ENV
+elif [ "$EVENT_NAME" == "release" ]; then
+  # relese tag will be something like this v1.1.1-prod
+  ENVIRONMENT=$(echo "$RELEASE_TAG" | awk -F'-' '{print $2}')
+  echo "ENVIRONMENT=$ENVIRONMENT" >> $GITHUB_ENV
+else
+  exit 1
+fi
